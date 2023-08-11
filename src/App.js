022d0787d89a0ca93f97a9ea99ed911dbc51a0e8
Rashid Risan <95292841+rashidrzn@users.noreply.github.com>
2023-08-11 08:42:21 +0530

    sivjdsvn

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>hello</h1>
+        <h3>Love you from Sri Lanka</h3>
       </header>
     </div>
   );
